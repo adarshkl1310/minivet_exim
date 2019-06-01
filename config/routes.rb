b998432a981b9b385dcab7ac88a7ad3631f11216
adarshkl1310 <shopprod1@gmail.com>
2019-06-01 15:20:05 +0530

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+root 'temps#index'
 get '/home' => 'temps#home'
 get '/blog' => 'temps#blog'
 get '/about' => 'temps#about'
